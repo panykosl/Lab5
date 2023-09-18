cf265eb55b8937512f5cee96c095f7d6e05097b3
First Commit <panykosl@mail.uc.edu>
2023-09-18 12:54:08 -0400

    Commit

--- src/BirthMonth_Lab5.java
+++ src/BirthMonth_Lab5.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
 public class BirthMonth_Lab5 {
-}
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int birthMonth = 0;
+
+        System.out.println("What is your birth month? ");
+        birthMonth = in.nextInt();
+        if (birthMonth <= 12) {
+            System.out.println("Your birth month is " + birthMonth);
+        }
+            else if (birthMonth > 12) {
+            System.out.println("You entered an incorrect month value: " + birthMonth);
+        }
+        }
+
+    }
+
--- src/PartyAffiliation_Lab5.java
+++ src/PartyAffiliation_Lab5.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
 public class PartyAffiliation_Lab5 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String rep = "R";
+        String dem = "D";
+        String ind = "I";
+        String oth = "O";
+        System.out.println("Please insert your party affliliation: ");
+        String line = in.nextLine();
+
+    }
 }
+
